18a57733aaa16a085a0b8cdf6988ddaee3d2747f
Quill Cronwall <quilljazz@gmail.com>
2022-05-09 20:06:47 -0700

    add 1. twosum in python

---
+++ algorithms/python/1. twosum.py
@@ -0,0 +1,28 @@
+# Source : https://leetcode.com/problems/two-sum/
+# Author : Quilleran Cronwall
+# Date   : 2022-05-09
+#******************************************************************
+
+"""
+INSTRUCTIONS:
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+"""
+# The algorithm acheives O(n) time complexity
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for i in range(len(nums)):
+            if nums[i] in hash_table:
+                return [hash_table[nums[i]], i]
+            else:
+                hash_table[target - nums[i]] = i
